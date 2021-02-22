7935d60573a59b0bc8db6d0a79def2e9a19a0cfa
Yaroslav Pidlubnyi <ybiuza3@gmail.com>
2021-02-22 15:30:38 +0100

    Added project to Git

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header =()=> {
+    return (
+        <header className="header">
+            <div className="left_img">
+                <img src="https://w7.pngwing.com/pngs/437/512/png-transparent-check-mark-computer-icons-right-or-wrong-miscellaneous-angle-text.png" alt=""/>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+
+const Profile = () =>{
+    return (
+        <div className="content">
+            <img className="img_back_profile" src="http://i.imgur.com/um8pJ5M.png" alt=""/>
+            <div className="av_descr">
+                <img src="https://res.cloudinary.com/blab/image/upload/c_fill,g_center,h_250,w_250/v1/production/user/3514/gallery/ok3x3ydgnzzexdqpckdo" alt=""/>
+                avatar +descr
+            </div>
+            <div>
+                my post
+                <div>
+                    New Post
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
